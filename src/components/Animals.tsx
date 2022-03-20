3253ef82bf6995f7de1c6dbd2d38aba1f972552d
tovehydmark <tovehydmark@gmail.com>
2022-03-20 18:00:25 +0100

    removed services, fetch in Animals component

--- src/components/Animals.tsx
+++ src/components/Animals.tsx
@@ -1,4 +1,18 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { IAnimal } from "../models/IAnimal";
+
 export function Animals() {
+  const [animals, setAnimals] = useState<IAnimal[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<IAnimal[]>(`https://animals.azurewebsites.net/api/animals`)
+      .then((response) => {
+        setAnimals(response.data);
+      });
+  }, []);
+
   return (
     <>
       <p>Aminals works</p>
