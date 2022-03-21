6336f878cc79b86f00571868252bda1cd9d12251
tovehydmark <tovehydmark@gmail.com>
2022-03-21 20:18:15 +0100

    prints extanded animal info and toggle  feed btn

--- src/components/AnimalInfo.tsx
+++ src/components/AnimalInfo.tsx
@@ -1,9 +1,48 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { IAnimal } from "../models/IAnimal";
+
 interface IAnimalInfoProps {
   id: number;
 }
 
 export function AnimalInfo(props: IAnimalInfoProps) {
+  const [animals, setAnimals] = useState<IAnimal[]>([]);
+
+  const [feed, setFeed] = useState<boolean>(false);
+
+  useEffect(() => {
+    axios
+      .get<IAnimal[]>(`https://animals.azurewebsites.net/api/animals`)
+      .then((response) => {
+        setAnimals(response.data);
+      });
+  }, []);
+
+  function hasBeenFed() {
+    setFeed(!feed);
+    console.log(feed);
+  }
+
+  let animalsList = animals.map((animal: IAnimal) => {
+    if (animal.id === props.id) {
+      return (
+        <div key={animal.id}>
+          <h1>{animal.name}</h1>
+          <p>Latin-namn: {animal.latinName}</p>
+          <p>Födelsedag: {animal.yearOfBirth}</p>
+          <p>{animal.longDescription}</p>
+          <img src={animal.imageUrl} alt="" width={100} height={100} />
+
+          <button onClick={hasBeenFed}>Mata</button>
+        </div>
+      );
+    }
+  });
   return (
-    <>{<div>Animal-Id från förälderkomponenten Animal = {props.id}</div>}</>
+    <>
+      {<div>Animal-Id från förälderkomponenten Animal = {props.id}</div>}
+      {animalsList}
+    </>
   );
 }
